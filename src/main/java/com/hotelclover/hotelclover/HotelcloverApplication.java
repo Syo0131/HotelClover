113b6ce2e37b2ef667d095dbec2d220e60711406
Syo <diazdaybelsamuel@gmail.com>
2025-09-24 22:28:45 -0400

    first commit

---
+++ src/main/java/com/hotelclover/hotelclover/HotelcloverApplication.java
@@ -0,0 +1,13 @@
+package com.hotelclover.hotelclover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelcloverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelcloverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotelclover/hotelclover/HotelcloverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelclover.hotelclover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelcloverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
